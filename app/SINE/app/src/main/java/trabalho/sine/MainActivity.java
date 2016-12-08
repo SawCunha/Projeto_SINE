86a7138aaa596c59ddd09583637b09fdd5e326f2
wdmeida <wdmeida@gmail.com>
2016-12-08 13:22:12 -0200

    Classe utilizada para requisições refatorada para utilizar Volley.

--- app/SINE/app/src/main/java/trabalho/sine/MainActivity.java
+++ app/SINE/app/src/main/java/trabalho/sine/MainActivity.java
@@ -2,6 +2,10 @@ package trabalho.sine;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+import trabalho.sine.controller.RequestURL;
+import trabalho.sine.model.VolleyCallback;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RequestURL.requestURL("https://cinevertentes.herokuapp.com/api/v1/cineplaza/movies", this, new VolleyCallback() {
+            @Override
+            public void onSuccess(String response) {
+                Toast.makeText(MainActivity.this, response, Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
