c5da08853957d629b4035a94f34a009148beb611
Mauricio Jr <mauriciojs.junior@gmail.com>
2016-12-09 16:53:34 -0200

    Tomei uma mijada do Armando e alterei para promises

--- api/app/controllers/vaga.js
+++ api/app/controllers/vaga.js
@@ -7,35 +7,43 @@ const utils = require('../resources/utils');
 module.exports = function (app) {
   let controller = {};
 
-  controller.getVaga = function (req, res, cidade, funcao, id) {
+  controller.getVaga = function (req, res) {
+    const cidade = req.params.cidade;
+    const funcao = req.params.funcao;
+    const id = req.params.vaga;
+
     let vaga = new Vaga({ id: id, cidade: cidade, funcao: funcao });
-    vaga = this.createVaga(req, res, vaga);
+    controller.createVaga(req, res, vaga);
   };
 
-  controller.createVaga = (req, res, vaga) => {
+  controller.createVaga = function (req, res, vaga) {
     let url = vaga.url_sine;
-    request(url, function (error, response, html) {
-      if (!error) {
+    request(url)
+    .then((html) => {
         let $ = cheerio.load(html);
         vaga.descricao = cleanString($('dd[itemprop="description"]').text());
         vaga.cidade = cleanString($('.label_cidade_resultado').text());
         vaga.empresa = cleanString($('.label_empresa_resultado').text());
         vaga.titulo = cleanString($('h1[itemprop="title"]').text());
         vaga.telefone = cleanString($('#ctl00_cphConteudo_ddTelefone').text());
-        try{
+        try {
           vaga.salario = cleanString($('#ctl00_cphConteudo_hplMediaSalarial').get(0).prev.data);
-        } catch(err){}
+        } catch (err) {
+          res.json('Erro na vaga');
+        }
 
         res.status(200).json(vaga);
-      }
-    });
+      }).catch((err) => {
+        console.error('Erro ao acessar URL: ' + url);
+        console.error(err);
+      });
   };
 
   controller.getVagas = (req, res) => {
     const funcao = req.query.funcao;
     const local = req.query.local;
 
-    if(!funcao && !local){
+    if (!funcao && !local) {
       res.status(400).json('Faltando Parametro(s).');
       return;
     }
@@ -50,9 +58,9 @@ module.exports = function (app) {
     })
     .catch((error) => {
       console.error(error);
-      res.status(404).json('Erro ao obter dados.')
+      res.status(404).json('Erro ao obter dados.');
     });
-  }
+  };
 
   return controller;
 };
--- api/app/routes/routes.js
+++ api/app/routes/routes.js
@@ -7,16 +7,7 @@ module.exports = (app) => {
       res.send('API SINE - TP ANDROID');
     });
 
-  app.get('/vaga/:cidade/:funcao?/:vaga?', (req, res) => {
-    const cidade = req.params.cidade;
-    const funcao = req.params.funcao;
-    const vaga = req.params.vaga;
-    if (cidade && funcao && vaga)
-      vagasController.getVaga(req, res, cidade, funcao, vaga);
-    else {
-      res.json('Listagem de vagas... Em construção');
-    }
-  });
+  app.get('/vaga/:cidade/:funcao/:vaga', vagasController.getVaga);
 
   // vagas?funcao={funcao}&local={local}
   // local = cidade/estado
