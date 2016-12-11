0366796419f11dd911478d29e7ea20bf56b792aa
wdmeida <wdmeida@gmail.com>
2016-12-11 20:28:23 -0200

    Refatoração de tela.

--- app/SINE/app/src/main/java/trabalho/sine/SearchForGraphicActivity.java
+++ app/SINE/app/src/main/java/trabalho/sine/SearchForGraphicActivity.java
@@ -1,16 +1,17 @@
 package trabalho.sine;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.Button;
 import android.widget.EditText;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
+import trabalho.sine.controller.RequestURL;
 
 public class SearchForGraphicActivity extends AppCompatActivity {
 
-    @BindView(R.id.search_button) Button searchButton;
     @BindView(R.id.cargo_search_edit_text) EditText cargoSearchEditText;
 
     @Override
@@ -20,5 +21,23 @@ public class SearchForGraphicActivity extends AppCompatActivity {
 
         ButterKnife.bind(this);
         ButterKnife.setDebug(true);
+    }
+
+    @OnClick(R.id.search_button)
+    public void pesquisarCargo(){
+
+        //Falta implementar a lógica para buscar os dados
+
+        RequestURL requestURL = new RequestURL(this);
+
+        requestURL.requestURL("blablabla", new RequestURL.VolleyCallback() {
+            @Override
+            public void onSuccess(String response) {
+                Intent graphicIntent = new Intent(SearchForGraphicActivity.this, GraphicActivity.class);
+                startActivity(graphicIntent);
+            }
+        });
+
+
     }
 }
