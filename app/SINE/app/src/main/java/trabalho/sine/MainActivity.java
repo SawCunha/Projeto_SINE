6cd20bd58c3e809957e666996f6648169c7fb089
Dborah <deborahferreirafonseca@gmail.com>
2016-12-12 20:24:39 -0200

    Alterando funcionalidades da barra Toolbar

--- app/SINE/app/src/main/java/trabalho/sine/MainActivity.java
+++ app/SINE/app/src/main/java/trabalho/sine/MainActivity.java
@@ -76,11 +76,6 @@ public class MainActivity extends AppCompatActivity implements FragmentDrawer.Fr
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
         return super.onOptionsItemSelected(item);
     }
 
